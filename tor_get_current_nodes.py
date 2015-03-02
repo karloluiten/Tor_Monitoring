bf9b6b71ae994226c3ea35e8ed7bc41dbc69d518
KarloLuiten <github@karloluiten.nl>
2015-03-02 09:42:53 +0100

    Remove non standard syslog libs, remove daemon functions. Just put this python in a cronjob and it will send out syslog. Use rsyslog to filter these out/handle them/send to cef connector

--- tor_get_current_nodes.py
+++ tor_get_current_nodes.py
@@ -9,44 +9,25 @@ __email__ = "Justin@alienonesecurity.com"
 __status__ = "Production"
 
 
-import requests
+import urllib2
 import time
 import sys
-from run_service.daemon_service import *
-from syslog.syslog_tcp import *
 
-
-def get_tor_nodes(urls_list):
-    """
-    Get get current lists of tor exit and router nodes
-    :param urls_list: List of url strings
-    :return: CEF formatted event output TCP syslog
-    """
-    product_list = []
-    for url in urls_list:
-        get_request_url = requests.get(url)
-        if get_request_url.status_code == 200:
-            if 'exit' in url:
-                data = get_request_url.text
-                tor_exit_list = data.encode('utf-8').split('\n')
-                product_list.append(tor_exit_list)
-            if 'all' in url:
-                data = get_request_url.text
-                tor_router_list = data.encode('utf-8').split('\n')
-                product_list.append(tor_router_list)
+def get_tor_nodes(url):
+    response = urllib2.urlopen(url)
+    content = response.read()
+    product_list=[]
+    product_list += content.split("\n")
     return product_list
 
 
-def syslog_cef(product_list_obj):
-    sock = syslog_tcp_open('127.0.0.1', port=1026)
-    for element in product_list_obj[0]:
-        cef_exit_node = 'CEF:0|Tor Exit Node|Tor Exit|1.0|Exit Node|Tor Exit Node|1| src=%s' % element
-        syslog_tcp(sock, "%s" % cef_exit_node, priority=0, facility=7)
-    for element in product_list_obj[1]:
-        cef_router_node = 'CEF:0|Tor Router Node|Tor Router|1.0|Router Node|Tor Router Node|1| dst=%s' % element
-        syslog_tcp(sock, "%s" % cef_router_node, priority=0, facility=7)
+def syslog_cef(product_list,nodetype):
+    for element in product_list:
+        cef_exit_node = 'CEF:0|Tor Node|{0}|1.0|{0}|{0} Node|1| src={1}'.format(nodetype,element)
+        # DEBUG:
+        print cef_exit_node
+        syslog.syslog(cef_exit_node)
     time.sleep(0.01)
-    syslog_tcp_close(sock)
 
 
 def main():
@@ -55,40 +36,10 @@ def main():
     :return: None
     """
     start_time = time.time()
-    urls_list = ["http://torstatus.blutmagie.de/ip_list_exit.php",
-                 "http://torstatus.blutmagie.de/ip_list_all.php"]
-    while True:
-        syslog_cef(get_tor_nodes(urls_list))
-        time.sleep(86400 - ((time.time() - start_time) % 86400))
-
-
-class MyDaemon(Daemon):
-    """
-    Main instance run as daemon
-    """
-    def run(self):
-        while True:
-            main()
-            time.sleep(0.01)
+    syslog_cef( get_tor_nodes( "http://torstatus.blutmagie.de/ip_list_exit.php" ), "Tor Exit" )
+    syslog_cef( get_tor_nodes( "http://torstatus.blutmagie.de/ip_list_all.php" ), "Tor Router" )
 
 
 if __name__ == "__main__":
-    daemon = MyDaemon('/tmp/tor_monitor.pid')
-    if len(sys.argv) == 2:
-        print('{} {}'.format(sys.argv[0], sys.argv[1]))
-        if 'start' == sys.argv[1]:
-            daemon.start()
-        elif 'stop' == sys.argv[1]:
-            daemon.stop()
-        elif 'restart' == sys.argv[1]:
-            daemon.restart()
-        elif 'status' == sys.argv[1]:
-            daemon.status()
-        else:
-            print("Unknown command")
-            sys.exit(2)
-        sys.exit(0)
-    else:
-        print('Correct syntax: ')
-        print("Usage: {} start|stop|restart".format(sys.argv[0]))
-        sys.exit(2)
+  main()
+
